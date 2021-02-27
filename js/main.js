2e4cf9727cbb29007957e398364c9b8b6c31273a
Yauheni Mitskevich <62372524+evgeniy-web-dev@users.noreply.github.com>
2021-02-27 22:15:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        navSpeed: 1000,
+        items: 1,
+        nav: true,
+        loop: true
+        //navText: ['', '']
+    });
+  });
